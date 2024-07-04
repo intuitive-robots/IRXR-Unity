006fc574d666c6b03fe1fd5daa664a197962307d
JuliusJacobitz <julius@jacobitz.de>
2024-07-04 17:05:37 +0200

    add interaction rig and sample grabbable

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Unity.Collections;
 using UnityEngine;
