e9a40318ad56c8ab7ba89b3902f313c1250f3b86
Julius <haag.julius@outlook.de>
2024-07-12 13:46:05 +0200

    author : JJ - Static Scene Object grabbable' rm grabbable scenecontroller

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -22,10 +22,6 @@ public class SceneController : MonoBehaviour
     private float lastSimulationTimeStamp = 0.0f;
     public Dictionary<string, Transform> _objectsTrans;
 
-    public void Start() {
-        _makeGrabbable(gameObject);
-    }
-
     public void StartUpdate(Dictionary<string, Transform> objectsTrans) {
         _objectsTrans = objectsTrans;
     }
@@ -45,57 +41,6 @@ public class SceneController : MonoBehaviour
             _objectsTrans[name].position = transform.TransformPoint(new Vector3(value[0], value[1], value[2]));
             _objectsTrans[name].rotation = transform.rotation * new Quaternion(value[3], value[4], value[5], value[6]);
         }
-    }
-
-    private void _makeGrabbable(GameObject go)
-    {   // rigidbody
-        Rigidbody rb = go.AddComponent<Rigidbody>();
-        rb.isKinematic = true;
-        rb.useGravity = false;
-
-
-        // box collider
-        BoxCollider bc = go.AddComponent<BoxCollider>();
-        bc.size.Set(2, 2, 2);
-
-
-        // grab free transofrmer + constraints
-        GrabFreeTransformer gft = go.AddComponent<GrabFreeTransformer>();
-        TransformerUtils.RotationConstraints rotationConstraints =
-        new TransformerUtils.RotationConstraints()
-        {
-            XAxis = new TransformerUtils.ConstrainedAxis(),
-            YAxis = new TransformerUtils.ConstrainedAxis(),
-            ZAxis = new TransformerUtils.ConstrainedAxis()
-        };
-
-        rotationConstraints.XAxis.ConstrainAxis = true;
-        rotationConstraints.YAxis.ConstrainAxis = false;
-        rotationConstraints.ZAxis.ConstrainAxis = true;
-
-        gft.InjectOptionalRotationConstraints(rotationConstraints);
-
-
-        // grabbable
-        Grabbable grabbable = go.AddComponent<Grabbable>();
-        grabbable.MaxGrabPoints = -1;
-        grabbable.InjectOptionalOneGrabTransformer(gft);
-
-
-        // collider surface 
-        ColliderSurface cs = go.AddComponent<ColliderSurface>();
-        cs.InjectCollider(bc);
-
-
-        // movement provider
-        MoveFromTargetProvider mp = go.AddComponent<MoveFromTargetProvider>();
-
-
-        // ray interactable
-        RayInteractable ri = go.AddComponent<RayInteractable>();
-        ri.InjectOptionalPointableElement(grabbable);
-        ri.InjectSurface(cs);
-        ri.InjectOptionalMovementProvider(mp);
     }
 
 }
