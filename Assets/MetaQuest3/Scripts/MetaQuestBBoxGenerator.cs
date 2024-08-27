eb169a2b4874cf573dd984554bfca959b9752bbe
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-08-27 17:01:08 +0200

    Create axis grabfreetransformers

--- Assets/MetaQuest3/Scripts/MetaQuestBBoxGenerator.cs
+++ Assets/MetaQuest3/Scripts/MetaQuestBBoxGenerator.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Oculus.Interaction;
+using UnityEngine.UIElements;
 
 class BBoxData {
     public List<float[]> data;
+}
+
+enum ScaleMode {
+    ALL,
+    X,
+    Y,
+    Z
 }
 
 public class MetaQuestBBoxGenerator : MonoBehaviour {
 
     [SerializeField] public GameObject boundingBoxPrefab;
     private List<GameObject> bboxList = new List<GameObject>();
+    private ScaleMode scaleMode = ScaleMode.ALL;
     
     private void Start() {
         IRXRNetManager _netManager = IRXRNetManager.Instance;
@@ -20,6 +30,58 @@ public class MetaQuestBBoxGenerator : MonoBehaviour {
         if (OVRInput.GetDown(OVRInput.Button.Two)) // button A locks / unlocks the scene
         {
             GenerateDefaultBBox();
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            switch (scaleMode)
+            {
+                case ScaleMode.ALL:
+                    scaleMode = ScaleMode.X;
+                    break;
+                case ScaleMode.X:
+                    scaleMode = ScaleMode.Y;
+                    break;
+                case ScaleMode.Y:
+                    scaleMode = ScaleMode.Z;
+                    break;
+                case ScaleMode.Z:
+                    scaleMode = ScaleMode.ALL;
+                    break;
+            }
+            Debug.Log("Scale Mode: " + scaleMode);
+            
+            TransformerUtils.FloatRange range = new TransformerUtils.FloatRange(){
+                Min = 1.0f,
+                Max = 1.0f
+            };
+            TransformerUtils.ScaleConstraints scaleConstraints = new TransformerUtils.ScaleConstraints()
+            {
+                XAxis = new TransformerUtils.ConstrainedAxis()
+                {
+                    ConstrainAxis = scaleMode == ScaleMode.Y || scaleMode == ScaleMode.Z,
+                    AxisRange = range
+                },
+                YAxis = new TransformerUtils.ConstrainedAxis()
+                {
+                    ConstrainAxis = scaleMode == ScaleMode.X || scaleMode == ScaleMode.Z,
+                    AxisRange = range
+                },
+                ZAxis = new TransformerUtils.ConstrainedAxis()
+                {
+                    ConstrainAxis = scaleMode == ScaleMode.X || scaleMode == ScaleMode.Y,
+                    AxisRange = range
+                },
+                ConstraintsAreRelative = true
+                
+            };
+        
+            foreach (var bbox in bboxList)
+            {
+                foreach (var grabFreeTransformer in bbox.GetComponentsInChildren<GrabFreeTransformer>())
+                {
+                    grabFreeTransformer.InjectOptionalScaleConstraints(scaleConstraints);
+                }
+            }
         }
     }
 
