e535fc1ecd1cf34014c1fcdfc97a98f4c698c74a
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-12-07 17:09:50 +0100

    qr code class for all the qr teleport

--- Assets/MetaQuest3/QR/Barcode/BarcodeReader.cs
+++ Assets/MetaQuest3/QR/Barcode/BarcodeReader.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Anaglyph.DisplayCapture.Barcodes
+namespace Anaglyph.BarCodes
 {
-	public class BarcodeReader : MonoBehaviour
+	public class BarCodeReader : MonoBehaviour
 	{
 		[Serializable]
 		private struct Results
@@ -33,7 +33,7 @@ namespace Anaglyph.DisplayCapture.Barcodes
 
 			public AndroidInterface(GameObject messageReceiver)
 			{
-				androidClass = new AndroidJavaClass("com.trev3d.DisplayCapture.BarcodeReader");
+				androidClass = new AndroidJavaClass("com.trev3d.DisplayCapture.BarCodeReader");
 				androidInstance = androidClass.CallStatic<AndroidJavaObject>("getInstance");
 				androidInstance.Call("setup", messageReceiver.name);
 			}
@@ -41,7 +41,7 @@ namespace Anaglyph.DisplayCapture.Barcodes
 			public void SetEnabled(bool enabled) => androidInstance.Call("setEnabled", enabled);
 		}
 
-		public event Action<IEnumerable<Result>> OnReadBarcodes = delegate { };
+		public event Action<IEnumerable<Result>> OnReadBarCodes = delegate { };
 
 		private AndroidInterface androidInterface;
 
@@ -62,16 +62,16 @@ namespace Anaglyph.DisplayCapture.Barcodes
 
 		private void OnDestroy()
 		{
-			OnReadBarcodes = delegate { };
+			OnReadBarCodes = delegate { };
 		}
 
 		// Called by Android 
 
 #pragma warning disable IDE0051 // Remove unused private members
-		private void OnBarcodeResults(string json)
+		private void OnBarCodeResults(string json)
 		{
 			Results results = JsonUtility.FromJson<Results>(json);
-			OnReadBarcodes.Invoke(results.results);
+			OnReadBarCodes.Invoke(results.results);
 		}
 #pragma warning restore IDE0051 // Remove unused private members
 	}
---
+++ Assets/SceneLoader/Scripts/QRSceneAlignment.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QRSceneAlignment : MonoBehaviour {
+
+    [Serializable]
+    public class QRSceneAlignmentData {
+        public string qrText;
+        public List<float> pos;
+        public List<float> euler;
+        public bool fixeZ;
+        public Vector3 GetPos() {
+            // TODO: add transform
+            return new Vector3(pos[0], pos[1], pos[2]);
+        }
+
+        public Quaternion GetRot() {
+            // TODO: add transform
+            return Quaternion.Euler(euler[0], euler[1], euler[2]);
+        }
+    }
+
+    [SerializeField] private GameObject indicator;
+    protected QRSceneAlignmentData _data;
+    public bool isTracingQR = false;
+    virtual public void StartQRTracing(QRSceneAlignmentData data) {
+        _data = data;
+        isTracingQR = true;
+    }
+
+    public void StopQRTracing() {
+        isTracingQR = false;
+    }
+
+    protected void SetSceneOrigin(Pose origin){
+        indicator.transform.position = origin.position;
+        indicator.transform.rotation = origin.rotation;
+        transform.position = origin.position;
+        transform.rotation = origin.rotation;
+    }
+}
+
