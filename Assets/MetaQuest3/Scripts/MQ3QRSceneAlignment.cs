e18cbe130258ff0afdf7dc83df2b1913886f6e61
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-01-02 18:38:59 +0100

    update to unity6

---
+++ Assets/MetaQuest3/Scripts/MQ3QRSceneAlignment.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using IRXR.Node;
+
+public class MQ3QRSceneAlignment : QRSceneAlignment
+{
+	private bool isTracking = false;
+
+	private void Update() {
+		if (isTracking) {
+			OnTrackingMotionController();
+		}
+	}
+
+	public override void StartQRTracking(QRSceneAlignmentData data)
+	{
+		isTracking = true;
+		ApplyOffset();
+	}
+
+	public override void StopQRTracking()
+	{
+		isTracking = false;
+	}
+
+	private void OnTrackingMotionController()
+	{
+		var activeController = OVRInput.GetActiveController();
+		if (activeController != OVRInput.Controller.Touch) return;
+        Vector3 leftPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+		Vector3 rightPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+		transform.position = (leftPos + rightPos) / 2;
+		Quaternion leftRot = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+		Quaternion rightRot = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+        Vector3 euler = Quaternion.Slerp(leftRot, rightRot, 0.5f).eulerAngles;
+		transform.rotation = Quaternion.Euler(0, euler.y, 0);
+	}
+
+}
