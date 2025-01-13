5220caeb90b7a7dc30868f558d7edc83c72ba753
Ahmad Baki <35337080+ahmad-baki@users.noreply.github.com>
2025-01-13 16:18:44 +0100

    - Added MjpegServer

---
+++ Assets/MetaQuest3/Scripts/ScreenRecording/MjpegServer.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+public class MjpegServer : MonoBehaviour
+{
+    // public Camera streamCamera;
+    public Texture2D frameTexture;
+    public int serverPort = 8080;
+
+    private HttpListener httpListener;
+    private bool isRunning = false;
+    // private Texture2D _frameTexture;
+    private RenderTexture renderTex;
+
+    void Start()
+    {
+        // Set up the RenderTexture and Texture2D
+        // renderTex = new RenderTexture(640, 360, 24);
+        // streamCamera.targetTexture = renderTex;
+        // _frameTexture = new Texture2D(renderTex.width, renderTex.height, TextureFormat.RGB24, false);
+
+        // Start HTTP server
+        httpListener = new HttpListener();
+        httpListener.Prefixes.Add($"http://*: {serverPort}/");
+        httpListener.Start();
+        isRunning = true;
+        ThreadPool.QueueUserWorkItem(ListenerLoop);
+    }
+
+    void OnDestroy()
+    {
+        isRunning = false;
+        httpListener.Stop();
+    }
+
+    private void ListenerLoop(object state)
+    {
+        while (isRunning)
+        {
+            HttpListenerContext context = httpListener.GetContext();
+            HttpListenerResponse response = context.Response;
+
+            response.ContentType = "multipart/x-mixed-replace; boundary=frame";
+            response.StatusCode = (int)HttpStatusCode.OK;
+
+            using (Stream outputStream = response.OutputStream)
+            {
+                while (isRunning && outputStream.CanWrite)
+                {
+                    // // Read pixels from the camera
+                    // RenderTexture.active = renderTex;
+                    // _frameTexture.ReadPixels(new Rect(0,0, renderTex.width, renderTex.height), 0,0);
+                    // _frameTexture.Apply();
+
+                    byte[] jpg = frameTexture.EncodeToJPG(); // _frameTexture.EncodeToJPG();
+
+                    // Write the boundary
+                    string header = "\r\n--frame\r\n" +
+                                    "Content-Type: image/jpeg\r\n" +
+                                    "Content-Length: " + jpg.Length + "\r\n\r\n";
+                    byte[] headerBytes = System.Text.Encoding.UTF8.GetBytes(header);
+
+                    try
+                    {
+                        outputStream.Write(headerBytes, 0, headerBytes.Length);
+                        outputStream.Write(jpg, 0, jpg.Length);
+                        outputStream.Flush();
+                    }
+                    catch
+                    {
+                        // Client disconnected
+                        break;
+                    }
+
+                    Thread.Sleep(50); // ~20 FPS
+                }
+            }
+        }
+    }
+}
