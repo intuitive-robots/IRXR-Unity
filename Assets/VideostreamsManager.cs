9eae6ad2d2f11b47fb5caf5822c0e7f00c69b87d
bakia <usjuy@student.kit.edu>
2025-07-21 11:02:52 +0200

    Added Video-Streaming Services to code - works with CreateVideoStream, UpdateVideoStream, RemoveVideoStream - Services - Added VideostreamsManager-prefab - Added VideostreamsManager.cs-script

---
+++ Assets/VideostreamsManager.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using IRXR.Node;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VideostreamsManager : MonoBehaviour
+{
+    public Canvas canvas; // Reference to the Canvas where video streams will be displayed
+    private Dictionary<string, RawImage> videoStreams;
+    private Service<CreateVideoStreamRequest, bool> createVideoStreamService;
+    private Service<UpdateVideoStreamRequest, bool> updateVideoStreamService;
+    private Service<string, bool> removeVideoStreamService;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        videoStreams = new Dictionary<string, RawImage>();
+        createVideoStreamService = new Service<CreateVideoStreamRequest, bool>(
+            "CreateVideoStream",
+            (request) => CreateVideoStream(request.VideoStreamId, request.Position, request.Rotation)
+        );
+        updateVideoStreamService = new Service<UpdateVideoStreamRequest, bool>(
+            "UpdateVideoStream",
+            (request) => UpdateVideoStream(request.VideoStreamId, request.ImageData)
+        );
+        removeVideoStreamService = new Service<string, bool>(
+            "RemoveVideoStream",
+            (videoStreamId) => RemoveVideoStream(videoStreamId)
+        );
+
+    }
+
+
+    public bool CreateVideoStream(string videoStreamId, Vector3 position, Quaternion rotation)
+    {
+        if (videoStreams.ContainsKey(videoStreamId))
+        {
+            Debug.LogWarning($"Video stream with ID {videoStreamId} already exists.");
+            return false; // Return false if the video stream already exists
+        }
+
+        // Logic to add a video stream
+        Debug.Log($"Video stream '{videoStreamId}' added at position {position} with rotation {rotation}.");
+        // create a new RawImage and Canvas for the video stream
+        GameObject videoStreamObject = new GameObject(videoStreamId);
+        RawImage rawImage = videoStreamObject.AddComponent<RawImage>();
+        videoStreamObject.transform.SetParent(canvas.transform);
+        RectTransform rt = videoStreamObject.GetComponent<RectTransform>();
+        rt.SetParent(canvas.transform, false);
+        rawImage.SetNativeSize();
+        videoStreamObject.transform.SetPositionAndRotation(position, rotation);
+        videoStreams[videoStreamId] = rawImage;
+        return true; // Return true if successful
+    }
+
+    /// <summary>
+    /// Updates an existing video stream with new image data.
+    /// </summary>
+    /// <param name="videoStreamId">The ID of the video stream to update.</param>
+    /// <param name="imageData">The new JPG/PNG image data for the video stream.</param>
+    /// <returns>True if the video stream was updated successfully; otherwise, false.</returns>
+    public bool UpdateVideoStream(string videoStreamId, byte[] imageData)
+    {
+        // Logic to update a video stream
+        Debug.Log($"Video stream '{videoStreamId}' updated with new image data.");
+        if (videoStreams.TryGetValue(videoStreamId, out RawImage rawImage))
+        {
+            // Update the texture of the RawImage with the new image data
+            Texture2D texture = new Texture2D(2, 2);
+            // https://docs.unity3d.com/530/Documentation/ScriptReference/Texture2D.LoadImage.html
+            texture.LoadImage(imageData);
+
+            rawImage.texture = texture;
+            return true; // Return true if successful
+        }
+        return false; // Return false if the video stream was not found
+    }
+
+    public bool RemoveVideoStream(string videoStreamId)
+    {
+        // Logic to remove a video stream
+        Debug.Log($"Video stream '{videoStreamId}' removed.");
+        return videoStreams.Remove(videoStreamId);
+    }
+
+    // Request classes for video stream services
+    public class CreateVideoStreamRequest
+    {
+        public string VideoStreamId { get; set; }
+        public Vector3 Position { get; set; }
+        public Quaternion Rotation { get; set; }
+    }
+
+    public class UpdateVideoStreamRequest
+    {
+        public string VideoStreamId { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
